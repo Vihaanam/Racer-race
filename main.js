16c4cd4d3ade98ca46467e342fc0e44b3f6c4347
Vihaanam <77443799+Vihaanam@users.noreply.github.com>
2021-04-09 13:51:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 canvas = document.getElementById("myCanvas");
 ctx = canvas.getContext("2d");
+winner_winner = "none";
+winner = "";
 car_width = 100;
 car_height = 100;
 car_2_width = 100;
 car_2_height = 100;
 race_background = "depositphotos_22495909-stock-illustration-racing-horizontal-backdrop.jpg";
 car_background = "car1.png";
+car_2_background = "car2.png";
 car_x = 0;
 car_y = 60;
 car_2_x = 0;
@@ -19,7 +22,7 @@ function add() {
     car_new_image.src = car_background;
     car_2_new_image = new Image();
     car_2_new_image.onload = add_car_2_background;
-    car_2_new_image.src = car_background;
+    car_2_new_image.src = car_2_background;
 }
 function add_background() {
     ctx.drawImage(background_new_image, 0, 0, canvas.width, canvas.height);
@@ -31,6 +34,7 @@ function add_car_2_background() {
     ctx.drawImage(car_2_new_image, car_2_x, car_2_y, car_2_width, car_2_height);
 }
 window.addEventListener("keydown", my_keydown);
+//W(87) makes the car go right, S(83) makes the car go left, A(65) makes the car go up and D(40) makes it go down
 function my_keydown(e) {
     keyPress = e.keyCode;
     console.log(keyPress);
@@ -68,15 +72,19 @@ function my_keydown(e) {
     }
 }
 function up1() {
-    if (car_x <= 700) {
+    if (car_x <= 700 && winner_winner != "won") {
         car_x = car_x + Math.floor(Math.random()*10)+5;
         add_background();
         add_car_background();
         add_car_2_background();
+    } else {
+        winner_winner = "won";
+        winner = "Car 1 won!";
+        document.getElementById("result_result").innerHTML = winner;
     }
 }
 function down1() {
-    if (car_x >= 0) {
+    if (car_x >= 0 && winner_winner != "won") {
         car_x = car_x - Math.floor(Math.random()*10)-5;
         add_background();
         add_car_background();
@@ -84,7 +92,7 @@ function down1() {
     }
 }
 function left1() {
-    if (car_y >= 0) {
+    if (car_y >= 0 && winner_winner != "won") {
         car_y = car_y - Math.floor(Math.random()*10)-5;
         add_background();
         add_car_background();
@@ -92,7 +100,7 @@ function left1() {
     }
 }
 function right1() {
-    if (car_y <= 300) {
+    if (car_y <= 300 && winner_winner != "won") {
         car_y = car_y + Math.floor(Math.random()*10)+5;
         add_background();
         add_car_background();
@@ -100,15 +108,19 @@ function right1() {
     }
 }
 function up2() {
-    if (car_2_x <= 700) {
+    if (car_2_x <= 700 && winner_winner != "won") {
         car_2_x = car_2_x + Math.floor(Math.random()*10)+5;
         add_background();
         add_car_background();
         add_car_2_background();
+    } else {
+        winner_winner = "won";
+        winner = "Car 2 won!";
+        document.getElementById("result_result").innerHTML = winner;
     }
 }
 function down2() {
-    if (car_2_x >= 0) {
+    if (car_2_x >= 0 && winner_winner != "won") {
         car_2_x = car_2_x - Math.floor(Math.random()*10)-5;
         add_background();
         add_car_background();
@@ -116,7 +128,7 @@ function down2() {
     }
 }
 function left2() {
-    if (car_2_y >= 0) {
+    if (car_2_y >= 0 && winner_winner != "won") {
         car_2_y = car_2_y - Math.floor(Math.random()*10)-5;
         add_background();
         add_car_background();
@@ -124,7 +136,7 @@ function left2() {
     }
 }
 function right2() {
-    if (car_2_y <= 300) {
+    if (car_2_y <= 300 && winner_winner != "won") {
         car_2_y = car_2_y + Math.floor(Math.random()*10)+5;
         add_background();
         add_car_background();
